df6be865ac46be07bcd30efa8f3ac050326458ce
GMN-dev <g.magalhaes.dev@gmail.com>
2024-06-05 09:39:00 -0300

    configurada as cores do projeto

---
+++ src/app/login/page.jsx
@@ -0,0 +1,10 @@
+export default function Login(){
+    return(<>
+        <section className="">
+            <img src="logo-field.png" className="bg" />
+        </section>
+        <section>
+
+        </section>
+    </>)
+}
